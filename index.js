ece45fc2b16edc1bf5f9c637cf06e8a902901e83
k-koech <kelvin.koech@student.moringaschool.com>
2022-06-26 08:20:16 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Add your code here
+// DOGS
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  };
+
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  
+  fetch("http://localhost:3000/dogs", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  })
+  .catch(function (error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  });
+
+
+// USERS
+const usersObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+        name: "Kelvin",
+        email: "Koech",
+      }),
+  };
+  
+  fetch("http://localhost:3000/users", usersObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  })
+  .catch(function (error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  });
